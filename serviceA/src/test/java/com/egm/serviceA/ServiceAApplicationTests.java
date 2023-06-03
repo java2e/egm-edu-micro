7e5e766fff1ae7dc013c2dafed090a3db42d9a74
mehmetdemircioglu <112698694+mehmetdemircioglu@users.noreply.github.com>
2023-06-03 13:54:23 +0300

    microservices

---
+++ serviceA/src/test/java/com/egm/serviceA/ServiceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egm.serviceA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
